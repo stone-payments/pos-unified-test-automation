11cf1249bfab3d07cb8eb4091a8fa7a45d250009
Luciano Santos <luckvargas@gmail.com>
2018-01-22 15:53:09 -0200

    Implement other keyboard methods

--- src/actuators/keyboardcontroller.cpp
+++ src/actuators/keyboardcontroller.cpp
@@ -1,4 +1,6 @@
 #include "keyboardcontroller.h"
+#include <chrono>
+#include <thread>
 
 const float_t keyZOffset = 1;
 
@@ -10,10 +12,26 @@ KeyboardController::KeyboardController(QObject *parent) : QObject(parent)
 
 void KeyboardController::keyPress(Qt::Key key)
 {
+    this->keyDown(key);
+    this->keyUp();
+}
+
+void KeyboardController::keyPressAndHold(Qt::Key key, int milliseconds)
+{
+    this->keyDown(key);
+    std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+    this->keyUp();
+}
+
+void KeyboardController::keyDown(Qt::Key)
+{
     QPointF charPosition = m_visionWorkerInstance->getCharPosition(QChar(key));
     m_printerControllerInstance->setXYPosition(charPosition);
 
     m_printerControllerInstance->moveZ(keyZOffset);
-    m_printerControllerInstance->moveZ(-keyZOffset);
 }
 
+void KeyboardController::keyUp()
+{
+    m_printerControllerInstance->moveZ(-keyZOffset);
+}
